a9b02f43ac3978b7b5de937675713f7e7ea1a930
juan-patrick <juan-p.andrade@outlook.com>
2021-05-20 04:06:00 -0300

    feat: create model Titulacao

---
+++ src/models/Titulacao.js
@@ -0,0 +1,16 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Titulacao extends Model {
+  static init(sequelize) {
+    super.init({
+      nome_titulacao: {
+        type: DataTypes.STRING(45),
+        allowNull: false,
+      }
+    }, { sequelize })
+  }
+
+  static associate(models) { return models }
+}
+
+module.exports = Titulacao;
