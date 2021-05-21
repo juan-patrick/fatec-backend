73fb142fa2c4680791254eb8c385229bd21bd422
juan-patrick <juan-p.andrade@outlook.com>
2021-05-21 20:08:13 -0300

    fix: model duplicate key and name pattern

---
+++ src/models/Inatividade.js
@@ -0,0 +1,24 @@
+const { Model, DataTypes } = require('sequelize');
+
+class Inatividade extends Model {
+  static init(sequelize) {
+    super.init({
+      data_ini: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      data_fim: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      motivo: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+    }, { sequelize })
+  }
+
+  static associate(models) { return models }
+};
+
+module.exports = Inatividade;
