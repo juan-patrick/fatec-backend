9129d996bc76d3818169228aaa8193738cd9a15c
juan-patrick <juan-p.andrade@outlook.com>
2021-05-21 20:43:05 -0300

    feat: initiallize a new models

--- src/database/index.js
+++ src/database/index.js
@@ -3,16 +3,21 @@ const { Sequelize } = require('sequelize');
 const dbConfig = require('../config/database');
 const Aluno = require('../models/Aluno');
 const AreaPesquisa = require('../models/AreaPesquisa');
+const Certificado = require('../models/Certificado');
 const Comissao = require('../models/Comissao');
 const CursoExtensao = require('../models/CursoExtensao');
 const EmpregoPublico = require('../models/EmpregoPublico');
 const Endereco = require('../models/Endereco');
 const Horario = require('../models/Horario');
+const Inatividade = require('../models/Inatividade');
+const Professor = require('../models/Professor');
 const Regime = require('../models/Regime');
 const RegistroSubstituicao = require('../models/RegistroSubstituicao');
+const Staff = require('../models/Staff');
 const TipoContrato = require('../models/TipoContrato');
 const Titulacao = require('../models/Titulacao');
 const Turma = require('../models/Turma');
+const Vinculo = require('../models/Vinculo');
 
 const connection = new Sequelize(dbConfig);
 
@@ -28,6 +33,11 @@ RegistroSubstituicao.init(connection);
 TipoContrato.init(connection);
 Titulacao.init(connection);
 Turma.init(connection);
+Vinculo.init(connection);
+Staff.init(connection);
+Certificado.init(connection);
+Inatividade.init(connection)
+Professor.init(connection);
 
 connection.sync({ alter: true });
 
